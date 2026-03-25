e28f4c7a759ede8a624842b8be4fe52e854f32f4
iam-ismaeel <kasaliismail2@gmail.com>
2026-03-25 15:59:09 +0100

    modified outputs

--- terraform-modules/2-ecs/3-task-role.tf
+++ terraform-modules/2-ecs/3-task-role.tf
@@ -1,3 +1,15 @@
 resource "aws_iam_role" "task_role" {
   name = "${var.cluster_name}-task-role"
+assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
